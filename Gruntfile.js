13e2777673ca8c4ad71dcd46611849d46a2146da
Joshua Rountree <joshua@swodev.com>
2014-01-25 23:27:44 -0500

    initial commit - heavy development

---
+++ Gruntfile.js
@@ -0,0 +1,117 @@
+'use strict';
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      all: [
+        'Gruntfile.js',
+        'assets/js/*.js',
+        '!assets/js/application.js'
+      ]
+    },
+    less: {
+      dist: {
+        files: {
+          'assets/css/application.css': [
+            'assets/less/application.less'
+          ]
+        },
+        options: {
+          compress: true,
+          // LESS source map
+          // To enable, set sourceMap to true and update sourceMapRootpath based on your install
+          sourceMap: false,
+          sourceMapFilename: 'assets/css/main.min.css.map',
+          sourceMapRootpath: '/app/themes/roots/'
+        }
+      }
+    },
+    uglify: {
+      dist: {
+        files: {
+          'assets/js/application.js': [
+            'assets/js/vendor/jquery-1.11.0.js',
+            'assets/js/plugins/bootstrap/transition.js',
+            'assets/js/plugins/bootstrap/alert.js',
+            'assets/js/plugins/bootstrap/button.js',
+            'assets/js/plugins/bootstrap/carousel.js',
+            'assets/js/plugins/bootstrap/collapse.js',
+            'assets/js/plugins/bootstrap/dropdown.js',
+            'assets/js/plugins/bootstrap/modal.js',
+            'assets/js/plugins/bootstrap/tooltip.js',
+            'assets/js/plugins/bootstrap/popover.js',
+            'assets/js/plugins/bootstrap/scrollspy.js',
+            'assets/js/plugins/bootstrap/tab.js',
+            'assets/js/plugins/bootstrap/affix.js',
+            'assets/js/plugins/*.js',
+            'assets/js/app/app.js'
+          ]
+        },
+        options: {
+          // JS source map: to enable, uncomment the lines below and update sourceMappingURL based on your install
+          // sourceMap: 'assets/js/scripts.min.js.map',
+          // sourceMappingURL: '/app/themes/roots/assets/js/scripts.min.js.map'
+        }
+      }
+    },
+    watch: {
+      less: {
+        files: [
+          'assets/less/*.less',
+          'assets/less/bootstrap/*.less',
+          'assets/less/font-awesome/*.less',
+          'assets/less/responsive/large/*.less',
+          'assets/less/responsive/medium/*.less',
+          'assets/less/responsive/small/*.less',
+          'assets/less/responsive/tiny/*.less'
+        ],
+        tasks: ['less']
+      },
+      js: {
+        files: [
+          '<%= jshint.all %>',
+          'assets/js/app/*.js'
+        ],
+        tasks: ['jshint', 'uglify']
+      },
+      livereload: {
+        // Browser live reloading
+        // https://github.com/gruntjs/grunt-contrib-watch#live-reloading
+        options: {
+          livereload: false
+        },
+        files: [
+          'assets/css/application.css',
+          'assets/js/application.js'
+        ]
+      }
+    },
+    clean: {
+      dist: [
+        'assets/css/application.css',
+        'assets/js/application.js'
+      ]
+    }
+  });
+
+  // Load tasks
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-less');
+
+  // Register tasks
+  grunt.registerTask('default', [
+    'clean',
+    'less',
+    'uglify'
+  ]);
+  grunt.registerTask('dev', [
+    'watch'
+  ]);
+
+};
---
+++ assets/js/app/app.js
@@ -0,0 +1,282 @@
+(function() {
+  window.DDWRT = {};
+  DDWRT.ui = {};
+  DDWRT.legacy = {};
+  DDWRT.legacy.$header = $("#header");
+  DDWRT.domUtil = {};
+  DDWRT.domUtil.isTextNode = function() {
+    // If this is a text node, return true.
+    return( this.nodeType === 3 && $.trim($(this).text()).length > 0 );
+     
+  }
+  var $nav = DDWRT.ui.$nav = $("#menuMainList");
+  var $header = DDWRT.ui.$header = $('
+  <header role="banner" class="banner">
+    <div class="primary-nav navbar navbar-inverse navbar-static-top">
+      <div class="container">
+        <a href="/" class="navbar-brand">DD-WRT</a>
+        
+        <div class="navbar-header">
+          <button data-target=".navbar-collapse" data-toggle="collapse" class="navbar-toggle" type="button">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+        </div>
+        
+        <nav role="navigation" class="collapse navbar-collapse">
+        </nav>
+      </div>
+    </div>
+  </header>');
+  var $wrap = DDWRT.ui.$wrap = $("#wrapper");
+  var $content = DDWRT.ui.$content = $("#content");
+  //remove dd-wrt css
+  $('link[href*="style/"]').remove();
+
+  //BUILD OUT BOOTSTRAP3 STRUCTURE
+  //THIS SAVES US FROM HAVING TO MODIFY THE DD-WRT BUILD.
+  $header.prependTo('body');
+  $nav.addClass('nav navbar-nav');
+  $nav.find('a strong').contents().unwrap();
+  $nav.find('span strong').contents().unwrap();
+  $nav.find('span').wrap('<a href="#"></a>');
+  $nav.find('a span').contents().unwrap();
+  $nav.find('li.current')
+    .addClass('active');
+  $dropdowns = $nav.find('li > div > ul');
+  $dropdowns.unwrap().addClass('dropdown-menu');
+  $dropdowns.parents('li').addClass('dropdown');
+  $dropdowns.each(function() {
+    $this = $(this);
+    $this.prev('a')
+      .addClass('dropdown-toggle')
+      .attr('data-target','#')
+      .attr('data-toggle','dropdown')
+      .append(" <b class='caret'></b>");
+  });
+  $nav.appendTo('.primary-nav nav');
+
+  //remove legacy header
+  DDWRT.legacy.$header.remove();
+
+  //content areas
+  $wrap.addClass('wrap').attr('role','document');
+  $content.addClass('content')
+    .wrapInner('<main class="main" role="main"><div class="container"><div class="content-wrap"><div class="content-left"></div></div></div></main>');
+  $(".content-wrap").append('<div class="content-right"></div>');
+  var $subnav = $nav.find('li.current > .dropdown-menu').clone();
+  $subnav.attr('id','right-subnav').removeClass('dropdown-menu').addClass('nav');
+  $subnav.appendTo(".content-right");
+  
+  //set active submenu item based on h2 from title
+  var $page_title = $content.find('h2:first').clone().children().remove().end().text();
+  $subnav.find('a').each(function() {
+    var $item = $(this);
+    var $text = $item.clone().children().remove().end().text();
+    if ($page_title === $text) {
+      $item.parents('li').addClass('active');
+    }
+    //console.log($item.contents()($page_title));
+  });
+  $("form").addClass('form-horizontal');
+  $(".setting").each(function() {
+    var $inputWrap = $(this);
+    $inputWrap.find('script').remove();
+    $inputWrap
+      .addClass('form-group');
+
+    $inputWrap
+      .wrapInner('<div class="form-control-wrap"></div>');
+
+    var $controlWrap = $inputWrap.find('.form-control-wrap');
+    
+    var $labelWrap = $inputWrap.find('.label');
+    $labelWrap.wrapInner('<label class="control-label"></label>');
+    $labelWrap.prependTo($inputWrap);
+    var $label = $inputWrap.find('label');
+    $label.unwrap();
+
+    //set help-text
+    $controlWrap.find('span.default').addClass('help-block');
+    //move hidden elements outside of controlWrap
+    $controlWrap.find("input[type=hidden]").appendTo($inputWrap);
+    //check if contains radio
+    var radio_count = $controlWrap.find('input[type=radio]').length;
+    if($controlWrap.find('input[type=radio]').length) {
+      $controlWrap.find('input[type=radio]').each(function() {
+        $(this).add(this.nextSibling).wrapAll('<label/>');
+        //if(radio_count > 1) {
+          $(this).parents('label').addClass('radio-inline');
+        // } else {
+        //   $controlWrap.addClass('radio');
+        // }
+      });
+    }
+    
+    var checkbox_count = $controlWrap.find('input[type=checkbox]').length;
+    if(checkbox_count) {
+      $controlWrap.find('input[type=checkbox]').each(function() {
+        $(this).add(this.nextSibling).wrapAll('<label/>');
+        //if(checkbox_count > 1) {
+          $(this).parents('label').addClass('checkbox-inline');
+        // } else {
+        //   $controlWrap.addClass('checkbox');
+        // }
+      });
+    }
+
+    $inputWrap
+      .find('input[type=text],input[type=password]')
+      .addClass('form-control');
+
+    //fix class="num" inputs
+    var $numInputs = $inputWrap
+      .find('input.num');
+
+    numInputsCount = $numInputs.length;
+    if(numInputsCount > 0) {
+      var textNodes = $controlWrap.contents().filter(DDWRT.domUtil.isTextNode);
+      //console.log(textNodes);
+      $numInputs.each(function() {
+        var $numInput = $(this);
+
+        //check for prefix or suffix text on single input.
+        if(numInputsCount == 1) {
+          if(textNodes.length > 0) {
+            //add class to controlWrap for input-group
+            $controlWrap.addClass('input-group');
+          }
+          $(textNodes).each(function() {
+            var $node = $(this);
+            var text = $.trim($node.text());
+            if(text.length > 0) {
+              $node.wrap('<span class="input-group-addon"></span>');
+            }
+          });
+          // if(nextNode && nextNode.nodeType == 3) {
+          //   //has text suffix
+          //   $(nextNode).wrapAll("<span class='num-suffix'></span>");
+          // }
+
+          // if(prevNode && prevNode.nodeType == 3) {
+          //   //has text prefix
+          //   $(prevNode).wrapAll("<span class='num-prefix'></span>");
+          // }
+        }
+        //check for multiple number inputs
+        // and divider such as period's for ipv4
+        if(numInputsCount > 1) {
+          $controlWrap.addClass('input-group');
+          $divider = $(this.nextSibling)
+          if($divider.text() == '.') {
+            //assume IP or something...
+            $divider.wrap('<span class="input-group-addon"/>');
+            $controlWrap.find('.input-group-addon').css({
+              'border-left':0,
+              'border-right':0
+            });
+
+
+          }
+        }
+        
+        $numInput.attr('type','text').addClass('form-control');
+      });
+    }
+
+    //fix textareas
+    var $textareas = $inputWrap.find('textarea');
+    var textareasCount = $textareas.length
+
+    if(textareasCount > 0) {
+      $textareas.each(function() {
+        var $textarea = $(this);
+
+        $textarea.addClass('form-control');
+      });
+    }
+
+    //fix selects
+    var $selects = $inputWrap.find('select');
+    var selectsCount = $selects.length
+
+    if(selectsCount > 0) {
+      $selects.each(function() {
+        var $select = $(this);
+
+        $select.addClass('form-control');
+      });
+    }
+    //fix buttons
+    var $buttons = $inputWrap.find('input[type=button]');
+    var buttonsCount = $buttons.length
+
+    if(buttonsCount > 0) {
+      $buttons.each(function() {
+        var $button = $(this);
+
+        $button.addClass('btn btn-default btn-block');
+      });
+    }
+
+    //fix rest of inputs without a type attribute.
+    var $inputs = $controlWrap.find('input');
+    $inputs.each(function() {
+      var $input = $(this);
+      var attr = $input.attr('type');
+
+      // For some browsers, `attr` is undefined; for others,
+      // `attr` is false.  Check for both.
+      if (typeof attr == 'undefined' || attr == false) {
+          //if it doesn't have a type... add class form-control just to be safe..
+          $input.addClass('form-control');
+      }
+    });
+    
+  });
+
+  //fix form submit buttons 
+  var $formButtonWrap = $(".submitFooter");
+  $formButtonWrap.addClass('form-group');
+  //$formButtonWrap.wrapInner('<div class="btn-group"></div>');
+  $formButtonWrap.find('script').remove();
+  $formButtonWrap.prepend('<label class="control-label"></label>');
+  var $formButtons = $formButtonWrap.find('input[type=button]');
+
+  $formButtons.each(function() {
+    var $btn = $(this);
+    $btn.addClass('btn');
+    $btn.after(' ');
+     //fix save button
+    if($btn.attr('name') === 'save_button') {
+      $btn.addClass('btn-primary');
+    }
+    //fix apply button
+    else if($btn.attr('name') === 'apply_button') {
+      $btn.addClass('btn-default');
+    }
+
+    //fix cancel button
+    else if($btn.attr('name') === 'cancel_button') {
+      $btn.addClass('btn-default');
+    }
+
+    //fix reboot button
+    else if($btn.attr('name') === 'reboot_button') {
+      $btn.addClass('btn-danger');
+    }
+    else {
+      $btn.addClass('btn-default');
+    }
+  });
+  //console.log($current_subnav_item);
+  // DDWRT.ui.$nav
+  //   .addClass('navbar navbar-default navbar-fixed-top')
+  //   .attr('role','navigation')
+  //   .wrapInner('<div class="container"></div>');
+
+  //DDWRT.ui.$nav.find('.container').prepend(DDWRT.ui.$navbarHeader);
+
+})();
